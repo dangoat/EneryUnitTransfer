b9d49e75b5f33c2698d1616d3785c2865d120502
Daniel Evans <danevans@terpmail.umd.edu>
2017-07-21 11:44:39 -0400

    Update ui_building.js

--- public/js/ui_building.js
+++ public/js/ui_building.js
@@ -156,6 +156,8 @@ function build_user_panels(data) {
 				if (dragged_owner_id != dropped_owner_id) {										//only transfer marbles that changed owners
 					$(ui.draggable).addClass('invalid bounce');
 					transfer_marble(marble_id, dropped_owner_id);
+					var obj = {type: 'delete_marble', id: marble_id, v: 1};
+					ws.send(JSON.stringify(obj));
 					return true;
 				}
 			}
